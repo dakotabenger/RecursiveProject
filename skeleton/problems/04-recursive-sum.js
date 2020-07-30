5d03c4be3965c7022ab48a2f6beaae6bb7c5a986
root <root@DESKTOP-LE7491G.localdomain>
2020-07-30 13:28:50 -0700

    Fourth Exercise Completed

--- skeleton/problems/04-recursive-sum.js
+++ skeleton/problems/04-recursive-sum.js
@@ -9,9 +9,15 @@ sum([1, 2, 3]); // => 6
 sum([0, 1, -3]); // => -2
 sum([1, 2, 3, 4, 5]); //=> 15
 ***********************************************************************/
-
+// Base case: array.length === 0
+// Recursive Case: arr[0] + sum(arr.slice(1))
 function sum(arr) {
-
+    let totalSum = 0;
+    if (arr.length === 0) {
+        return totalSum;
+    }
+    totalSum = arr[0] + sum(arr.slice(1));
+    return totalSum;
 }
   
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
